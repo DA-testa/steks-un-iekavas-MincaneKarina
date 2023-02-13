31b501096d1f3ecd5beddf471799f245d2722ce5
MincaneKarina <karina.mincane@edu.rtu.lv>
2023-02-13 11:34:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -14,10 +14,16 @@ def find_mismatch(text):
     for i, next in enumerate(text):
         if next in "([{":
             # Process opening bracket, write your code here
+            opening_brackets_stack.append(Bracket, i + 1)
+            
             pass
 
         if next in ")]}":
             # Process closing bracket, write your code here
+            #if opening_brackets_stack.empty() or opening_brackets_stack.top().are_matching(next):
+            
+            #pass
+            #opening_brackets_stack.pop()
             pass
 
 
@@ -25,7 +31,11 @@ def main():
     text = input()
     mismatch = find_mismatch(text)
     # Printing answer, write your code here
-
+    if (mismatch):
+        print(mismatch)
+    else:
+        print("Success")
+    
 
 if __name__ == "__main__":
     main()
