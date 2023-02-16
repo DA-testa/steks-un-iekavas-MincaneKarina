d7429dc5906fc7382cac301255767ebe19eac389
MincaneKarina <karina.mincane@edu.rtu.lv>
2023-02-16 13:00:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def find_mismatch(text):
              return i+1
             opening_brackets_stack.pop()
         if opening_brackets_stack :
-            return opening_brackets_stack[0].position
+            return opening_brackets_stack[i].position
         return "Success"
             
         
