9f5d3674d094a07eaabeb02b44bdf4f190f67945
MincaneKarina <karina.mincane@edu.rtu.lv>
2023-02-17 09:10:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -22,16 +22,15 @@ def find_mismatch(text):
         # Process closing bracket, write your code here
             if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char, next):   
                 return i+1
+                
             opening_brackets_stack.pop()
-         
             if opening_brackets_stack:        
-                return opening_brackets_stack[0].position -1
-        return "Success"
+                return opening_brackets_stack[0].position 
+            return "Success"
        
 
 
 def main():
-    t = input()
     text = input()
     mismatch = find_mismatch(text)
     # Printing answer, write your code here
