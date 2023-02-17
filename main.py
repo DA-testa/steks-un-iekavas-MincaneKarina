2830e177e3091c2eca3b87e030ece70da867f9b9
MincaneKarina <karina.mincane@edu.rtu.lv>
2023-02-17 08:38:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -20,13 +20,10 @@ def find_mismatch(text):
 
         if next in ")]}":
         # Process closing bracket, write your code here
-            
-                
-                
-            if are_matching(opening_brackets_stack[-1].char, next):
-                    
-                opening_brackets_stack.pop()
-            return i+1
+            if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char, next):   
+                return i+1
+            opening_brackets_stack.pop()
+         
             if not opening_brackets_stack:        
                 return "Success"
             return opening_brackets_stack[0].position
